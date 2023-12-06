22e560bdb5913adb7afa192841da6e0706ec9465
afrodisac <eric.gericke2@gmail.com>
2023-12-06 22:12:19 +0100

    Started on day5 :(

--- Day5/p1.py
+++ Day5/p1.py
@@ -1,13 +1,42 @@
 import re
 
+
+# class mapping:
+#     def __init__(self, name, raw_mapping):
+#         self.name = name
+#         self.raw_mapping = raw_mapping
+
+
+
+
 def main():
     path = "eg.txt"
     lines = parser(path)
-    
-    for line in lines:
-        line = re.findall(r"\d+", line)
-        print(line)
 
+    mappings = {
+        "seeds": [],
+        "seed-to-soil map:":[],
+        "soil-to-fertilizer map:":[],
+        "fertilizer-to-water map:":[],
+        "water-to-light map:":[],
+        "light-to-temperature map:":[],
+        "temperature-to-humidity map:":[],
+        "humidity-to-location map:":[],
+                }
+    for line in lines:
+        name = ""
+        items = []
+        if line =="\n":
+            continue
+        clean_line = re.findall(r".+", line)[0]
+        print(clean_line)
+        if clean_line[0] == "seeds":
+            mappings["seeds"] = re.findall(r"\d+")
+    print(mappings)
+            
+
+        
+        # print(line)
 
 
 
